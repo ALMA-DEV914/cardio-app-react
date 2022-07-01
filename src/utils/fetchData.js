88b2e7baf729f7a44536cc6f88289a82c3e5f84f
ALMA_DEV914 <aungonalna58@gamil.com>
2022-06-30 20:43:21 -0700

    fixed limit api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
-  url: 'https://exercisedb.p.rapidapi.com/exercises',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '5909038cffmshb5b68ad5959bc6fp18adcdjsn9fb5b0681c4d',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -10,10 +9,8 @@ export const exerciseOptions = {
 
 export const youtubeOptions = {
   method: 'GET',
-  url: 'https://youtube-search-and-download.p.rapidapi.com/video',
-  params: {id: 'dQw4w9WgXcQ'},
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '5909038cffmshb5b68ad5959bc6fp18adcdjsn9fb5b0681c4d',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
