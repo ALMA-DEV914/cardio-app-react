72f495254661f9997a67043fcc136a5cd59f922c
ALMA_DEV914 <aungonalna58@gamil.com>
2023-04-05 17:08:45 -0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,16 @@ import { Facebook, Instagram, Twitter } from '@mui/icons-material';
 
 
 const Footer = () => (
-  <Container maxWidth="xlg" mt="80px" className='footer'>
-    <Stack gap="20px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+  <div  className='footer'>
+  <Container>
+    <Stack >
       <div className='d-flex icons'>
      <Facebook/> <Twitter/><Instagram/>
      </div>
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '25px', xs: '20px' } }} mt="41px" textAlign="center" pb="20px">Copyright reserved@2022</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '22px', xs: '20px' } }} mt="41px" textAlign="center" pb="20px">Alma Braun <br></br>Copyright reserved@2022</Typography>
   </Container>
+  </div>
 );
 
 export default Footer;
